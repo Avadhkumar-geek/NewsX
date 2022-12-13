6ec7b636b2665df57d8a899ec056f0adfed59540
Avadhkumar-geek <avadhkachhadiya@gmail.com>
2022-12-13 23:54:57 +0530

    added dark theme

--- src/App.js
+++ src/App.js
@@ -1,29 +1,96 @@
 import "./App.css";
-import React from "react";
+import { React, useState } from "react";
 import NavBar from "./components/NavBar";
 import News from "./components/News";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
+    const [theme, setTheme] = useState("light-theme");
+    const [textTheme, setTextTheme] = useState("text-dark");
+
     return (
         <>
             <BrowserRouter>
-                <NavBar />
+                <NavBar
+                    theme={theme}
+                    textTheme={textTheme}
+                    setTheme={setTheme}
+                    setTextTheme={setTextTheme}
+                />
                 <Routes>
-                    <Route path="/" element={<News key="home" category="all" />} />
-                    <Route path="/business" element={<News key="business" category="business" />} />
+                    <Route
+                        path="/"
+                        element={
+                            <News key="home" category="all" theme={theme} textTheme={textTheme} />
+                        }
+                    />
+                    <Route
+                        path="/business"
+                        element={
+                            <News
+                                key="business"
+                                category="business"
+                                theme={theme}
+                                textTheme={textTheme}
+                            />
+                        }
+                    />
                     <Route
                         path="/entertainment"
-                        element={<News key="entertainment" category="movies" />}
+                        element={
+                            <News
+                                key="entertainment"
+                                category="movies"
+                                theme={theme}
+                                textTheme={textTheme}
+                            />
+                        }
                     />
-                    <Route path="/science" element={<News key="science" category="science" />} />
-                    <Route path="/sports" element={<News key="sports" category="sports" />} />
+                    <Route
+                        path="/science"
+                        element={
+                            <News
+                                key="science"
+                                category="science"
+                                theme={theme}
+                                textTheme={textTheme}
+                            />
+                        }
+                    />
+                    <Route
+                        path="/sports"
+                        element={
+                            <News
+                                key="sports"
+                                category="sports"
+                                theme={theme}
+                                textTheme={textTheme}
+                            />
+                        }
+                    />
                     <Route
                         path="/technology"
-                        element={<News key="technology" category="technology" />}
+                        element={
+                            <News
+                                key="technology"
+                                category="technology"
+                                theme={theme}
+                                textTheme={textTheme}
+                            />
+                        }
                     />
-                    <Route path="/travel" element={<News key="science" category="travel" />} />
+                    <Route
+                        path="/travel"
+                        element={
+                            <News
+                                key="science"
+                                category="travel"
+                                theme={theme}
+                                textTheme={textTheme}
+                            />
+                        }
+                    />
                 </Routes>
             </BrowserRouter>
         </>
